bda694d3728a17b9c7fc908cb755413c049edca6
Eveliny Muniz <evelinymuniz@gmail.com>
2024-01-04 16:00:09 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "a57", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
